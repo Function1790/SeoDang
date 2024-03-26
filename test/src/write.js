b9b3cd11ce608a126078ec8ccfab18149cc5a4f4
TerryFX <korean1790@gmail.com>
2024-03-27 01:56:51 +0900

    아이템 정보 & 로그인 & UI 수정

--- test/src/write.js
+++ test/src/write.js
@@ -14,3 +14,22 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+function getByID(idname) {
+    return document.getElementById(idname)
+}
+
+const title = getByID('title')
+const content = getByID('content')
+const select = getByID('select')
+const chooseFile = getByID('chooseFile')
+const price = getByID('price')
+
+function toData() {
+    return {
+        title: title.value,
+        content: content.value,
+        category: select.value !== "과목 선택" ? select.value : null,
+        price: price.value
+    }
+}
